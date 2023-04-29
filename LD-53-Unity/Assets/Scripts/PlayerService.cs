0610b9b5e8d4dcc59339617a78826f18b736c624
Alex Tagun <alt@highcore.io>
2023-04-30 00:44:48 +0400

    spawn player on grid

--- LD-53-Unity/Assets/Scripts/PlayerService.cs
+++ LD-53-Unity/Assets/Scripts/PlayerService.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class PlayerService : Service, IInject
+public class PlayerService : Service, IInject, IStart
 {
     private PlayerView _playerView;
-    
+    private AssetsCollection _assetsCollection;
+    private GridService _gridService;
+
     public void Inject()
     {
+        _assetsCollection = Services.Get<AssetsCollection>();
+        _gridService = Services.Get<GridService>();
         _playerView = Instantiate(Services.Get<AssetsCollection>().PlayerView);
-        
     }
 
     public void Move(Vector3 movePosition)
@@ -19,5 +22,13 @@ public class PlayerService : Service, IInject
     public IEnumerator MoveCrt(Vector3 movePosition)
     {
         yield return _playerView.Movement.Move(movePosition);
+    }
+
+    public void GameStart()
+    {
+        _playerView = Instantiate(_assetsCollection.PlayerView);
+        Vector3 pos = _gridService.GetWorldPoint(Vector2Int.zero);
+        pos.y = 1;
+        _playerView.transform.position = pos;
     }
 }
