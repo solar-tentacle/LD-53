36ca2250c203f521315dce65a13a365140b2d77b
Ivan <iw.lasgin2011@gmail.com>
2023-04-30 13:27:30 +0400

    add ui cards hand class

--- LD-53-Unity/Assets/Scripts/UI/UIHud.cs
+++ LD-53-Unity/Assets/Scripts/UI/UIHud.cs
@@ -5,4 +5,5 @@ public class UIHud : ActivateView
 {
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _clearButton;
+    [SerializeField] private UICardsHand _uiCardsHand;
 }
