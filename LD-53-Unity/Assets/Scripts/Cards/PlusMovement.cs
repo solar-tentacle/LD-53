9004153c31c28a08884399df74cea6e805c8b295
Alex Tagun <alt@highcore.io>
2023-04-30 15:27:02 +0400

    plus move refactoring

--- LD-53-Unity/Assets/Scripts/Cards/PlusMovement.cs
+++ LD-53-Unity/Assets/Scripts/Cards/PlusMovement.cs
@@ -10,7 +10,6 @@ public class PlusMovement : CardAction
     private PlayerView _playerView;
     private GridService _gridService;
     private List<GroundGridElement> _elements;
-    private Plane _plane = new(Vector3.up, 0);
 
     public override void Init()
     {
@@ -40,7 +39,7 @@ public class PlusMovement : CardAction
 
     public override bool CanExecute()
     {
-        if (TryGetMouseGridPos(out Vector2Int pos))
+        if (_gridService.TryGetMouseGridPos(out Vector2Int pos))
         {
             GroundGridElement element = _gridService.GetGroundView(pos);
             return _elements.Contains(element);
@@ -51,7 +50,7 @@ public class PlusMovement : CardAction
 
     public override IEnumerator Execute()
     {
-        if (TryGetMouseGridPos(out Vector2Int pos))
+        if (_gridService.TryGetMouseGridPos(out Vector2Int pos))
         {
             yield return _playerService.Move(pos);
         }
@@ -60,34 +59,11 @@ public class PlusMovement : CardAction
     private List<GroundGridElement> GetElements(Vector2Int pos)
     {
         List<GroundGridElement> elements = new();
-        TryAddElement(elements, pos + Vector2Int.up);
-        TryAddElement(elements, pos + Vector2Int.down);
-        TryAddElement(elements, pos + Vector2Int.left);
-        TryAddElement(elements, pos + Vector2Int.right);
+        _gridService.TryAddGroundElement(elements, pos + Vector2Int.up);
+        _gridService.TryAddGroundElement(elements, pos + Vector2Int.down);
+        _gridService.TryAddGroundElement(elements, pos + Vector2Int.left);
+        _gridService.TryAddGroundElement(elements, pos + Vector2Int.right);
 
         return elements;
-    }
-
-    private void TryAddElement(List<GroundGridElement> buffer, Vector2Int pos)
-    {
-        if (_gridService.TryGetGroundView(pos, out GroundGridElement element))
-        {
-            buffer.Add(element);
-        }
-    }
-
-    private bool TryGetMouseGridPos(out Vector2Int pos)
-    {
-        pos = Vector2Int.zero;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (_plane.Raycast(ray, out float distance))
-        {
-            Vector3 worldPosition = ray.GetPoint(distance);
-
-            pos = _gridService.GetMatrixPosition(worldPosition);
-            return true;
-        }
-
-        return false;
     }
 }
