698eda7f1c38667a420cb791c9c3c1fc3dc7729d
Ivan <iw.lasgin2011@gmail.com>
2023-04-30 19:21:27 +0400

    change level

--- LD-53-Unity/Assets/Scripts/UI/UIWinWindow.cs
+++ LD-53-Unity/Assets/Scripts/UI/UIWinWindow.cs
@@ -1,7 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class  UIWinWindow : ActivateView
 {
     [SerializeField] private Button _nextLevelButton;
+
+    private void Start()
+    {
+        _nextLevelButton.onClick.AddListener(OnNextButtonClicked);
+    }
+
+    private void OnNextButtonClicked()
+    {
+        Services.Get<GameFlowService>().GoToNextLevel();
+    }
 }
