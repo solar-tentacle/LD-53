731ad405e8f84776a54e66c140a4a7e83d9578f7
Ivan <iw.lasgin2011@gmail.com>
2023-05-01 21:09:43 +0400

    add pizza progress bar

--- LD-53-Unity/Assets/Scripts/GameConfig.cs
+++ LD-53-Unity/Assets/Scripts/GameConfig.cs
@@ -6,6 +6,7 @@ public class GameConfig : ScriptableObject
 {
     public int StartCoins;
     public int CoinsPerMove = 1;
+    public int StartPizzaCount = 5;
 
     public List<CardConfig> StartCards = new List<CardConfig>();
 
